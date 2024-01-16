a808772ee9fedb833719335933ccd6f047ae1627
lyubov <konysheva.lv@yandex.ru>
2024-01-16 14:19:11 +0300

    Еще правки.

---
+++ week_1/index.php
@@ -0,0 +1,9 @@
+<?php
+require('/week_1/src/functions.php');
+
+task1(['Маша', 'Ваня'], false);
+echo task2("*", 1, 2, 3);
+task3(8,8);
+task4();
+task5();
+task6();
--- week_1/src/functions.php
+++ week_1/src/functions.php
@@ -16,6 +16,7 @@ function task1($arr, $bool = false)
 <?php
 //Задание 2
 
+
 function task2(string $symbol, ...$args){
   foreach ($args as $arg) {
     if (is_int($args)) {
@@ -25,7 +26,7 @@ function task2(string $symbol, ...$args){
 
   switch ($symbol) {
     case '+':
-      return array_shift($args) + array_sum($args);
+      return $result1 = array_shift($args) + array_sum($args);
       case '-':
         return array_shift($args) - array_sum($args);
         case '/':
@@ -65,34 +66,41 @@ function task3(int $a, int $b){
   echo $result;
 }
 ?>
+<?php 
 
-<?php
 //Задание 4
 
+function task4 () {
 echo date ('d.n.Y. H:i');
 echo '<br>';
 echo strtotime('15.01.2024 00:00:00');
 echo '<br>';
-
+}
 ?>
+<?php 
 
-<?php
 //Задание 5
 
-$karl = 'Карл у Клары украл Кораллы';
-echo str_replace('К','', $karl);
-echo '<br>';
-$lemon = 'Две бутылки лимонада';
-echo str_replace('Две','Три', $lemon);
+function task5 () {
+  $karl = 'Карл у Клары украл Кораллы';
+  echo str_replace('К','', $karl);
+  echo '<br>';
+  $lemon = 'Две бутылки лимонада';
+  echo str_replace('Две','Три', $lemon);
 
+  }
 ?>
 
-<?php
-//Задание 6
-echo '<br>';
-file_put_contents('test.txt', 'Hello, again');
+<?php 
 
-$content = file_get_contents("test.txt");
-echo $content;
+//Задание 6
+
+function task6 () {
+  echo '<br>';
+  file_put_contents('test.txt', 'Hello, again');
+  
+  $content = file_get_contents("test.txt");
+  echo $content;
+  }
 
 ?>
